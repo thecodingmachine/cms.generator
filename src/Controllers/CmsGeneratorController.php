c0404cc7bc92fa24f0ab1f7d4773c094b59478e7
jbc <jb.charron@thecodingmachine.com>
2016-04-14 19:00:09 +0200

    Update

--- src/Controllers/CmsGeneratorController.php
+++ src/Controllers/CmsGeneratorController.php
@@ -1,23 +1,23 @@
 <?php
 namespace Mouf\Cms\Generator\Controllers;
 
+use Mouf\Controllers\AbstractMoufInstanceController;
+use Mouf\Database\Patcher\DatabasePatchInstaller;
+use Mouf\Database\TDBM\TDBMService;
+use Mouf\Html\Widgets\MessageService\Service\UserMessageInterface;
+use Mouf\InstanceProxy;
 use Mouf\Mvc\Splash\Controllers\Controller;
 use Mouf\Html\Template\TemplateInterface;
 use Mouf\Html\HtmlElement\HtmlBlock;
 use Psr\Log\LoggerInterface;
 use \Twig_Environment;
 use Mouf\Html\Renderer\Twig\TwigTemplate;
+use Zend\Diactoros\Response\RedirectResponse;
 
 /**
  * TODO: write controller comment
  */
-class CmsGeneratorController extends Controller {
-
-    /**
-     * The template used by this controller.
-     * @var TemplateInterface
-     */
-    public $template;
+class CmsGeneratorController extends AbstractMoufInstanceController {
 
     /**
      * The main content block of the page.
@@ -25,15 +25,142 @@ class CmsGeneratorController extends Controller {
      */
     public $content;
 
-    protected $selfedit;
-
     /**
      * @Action
      * @Logged
      */
-    public function index($selfedit = "false") {
+    public function index($name, $selfedit = "false") {
+
+        $this->initController($name, $selfedit);
+
         $this->selfedit = $selfedit;
         $this->content->addFile(__DIR__.'/../views/cmsGenerator.php', $this);
         $this->template->toHtml();
+    }
+
+    /**
+     * This action generates the DAOs and Beans for the TDBM service passed in parameter.
+     *
+     * @Action
+     *
+     * @param string $name
+     * @param string $componentName
+     * @param string $selfedit
+     *
+     * @return RedirectResponse
+     */
+    public function componentGenerate($name, $componentName, $selfedit = 'false')
+    {
+        $this->initController($name, $selfedit);
+
+        if(null == $componentName){
+            set_user_message("You must define a component name.", UserMessageInterface::ERROR);
+            return new RedirectResponse(ROOT_URL.'cmsadmin/?name='.$name);
+        }
+        self::sqlGenerate($componentName);
+
+        $uniqueName = "cms-generator-component-".$componentName;
+
+        DatabasePatchInstaller::registerPatch($this->moufManager,
+            $uniqueName,
+            "Creating the table for the new CMS component : ".$componentName,
+            "database/up/".date('YmdHis')."-patch.sql", // SQL patch file, relative to ROOT_PATH
+            "database/down/".date('YmdHis')."-patch.sql"); // Optional SQL revert patch file, relative to ROOT_PATH
+
+        $this->moufManager->rewriteMouf();
+
+        $patchService = new InstanceProxy('patchService', $selfedit == 'true');
+        $patchService->apply($uniqueName);
+
+        $daonamespace = $this->moufManager->getVariable('tdbmDefaultDaoNamespace');
+        $beannamespace = $this->moufManager->getVariable('tdbmDefaultBeanNamespace');
+        $daofactoryclassname = $this->moufManager->getVariable('tdbmDefaultDaoFactoryName');
+        $storeInUtc = $this->moufManager->getVariable('tdbmDefaultStoreInUtc');
+        $useCustomComposer = $this->moufManager->getVariable('tdbmDefaultUseCustomComposer');
+        $composerFile = $this->moufManager->getVariable('tdbmDefaultComposerFile');
+
+        $tdbmService = new InstanceProxy($name);
+        /* @var $tdbmService TDBMService */
+        $tdbmService->generateAllDaosAndBeans($daofactoryclassname, $daonamespace, $beannamespace, $storeInUtc, ($useCustomComposer ? $composerFile : null));
+
+        set_user_message("OK.", UserMessageInterface::SUCCESS);
+        return new RedirectResponse(ROOT_URL.'cmsadmin/?name='.$name);
+    }
+
+    /**
+     * This action generates the DAOs and Beans for the TDBM service passed in parameterd
+     *
+     * @param string $componentName
+     * @return string
+     */
+    public function sqlGenerate($componentName)
+    {
+        $rootPath = realpath(ROOT_PATH.'../../../').'/';
+
+        $upSqlFileName = "database/up/".date('YmdHis')."-patch.sql";
+        $downSqlFileName = "database/down/".date('YmdHis')."-patch.sql";
+
+        $baseDirUpSqlFile = dirname($rootPath.$upSqlFileName);
+        $baseDirDownSqlFile = dirname($rootPath.$downSqlFileName);
+
+        $upSql = "CREATE TABLE IF NOT EXISTS `".$componentName."` (
+                          `id` int(11) NOT NULL AUTO_INCREMENT,
+                          `title` varchar(255) NOT NULL DEFAULT '',
+                          `slug` varchar(255) NOT NULL DEFAULT '',
+                          `short_text` text NOT NULL DEFAULT '',
+                          `content` text NOT NULL DEFAULT '',
+                          `image` text NOT NULL DEFAULT '',
+                          PRIMARY KEY (`id`)
+                        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+
+        $downSql = "DROP TABLE IF EXISTS `".$componentName."`";
+
+        // Let's create the directory
+        if (!file_exists($baseDirUpSqlFile)) {
+            $old = umask(0);
+            $result = @mkdir($baseDirUpSqlFile, 0775, true);
+            umask($old);
+            if (!$result) {
+                set_user_message("Sorry, impossible to create directory '".plainstring_to_htmlprotected($baseDirUpSqlFile)."'. Please check directory permissions.");
+                //header('Location: .?name='.urlencode($name).'&selfedit='.urlencode($selfedit).($patchInstanceName ? '&patchInstanceName='.$patchInstanceName : ''));
+
+                return;
+            }
+        }
+
+        if (!is_writable($baseDirUpSqlFile)) {
+            set_user_message("Sorry, directory '".plainstring_to_htmlprotected($baseDirUpSqlFile)."' is not writable. Please check directory permissions.");
+            //header('Location: .?name='.urlencode($name).'&selfedit='.urlencode($selfedit).($patchInstanceName ? '&patchInstanceName='.$patchInstanceName : ''));
+
+            return;
+        }
+
+        // Let's create the directory
+        if (!file_exists($baseDirDownSqlFile)) {
+            $old = umask(0);
+            $result = @mkdir($baseDirDownSqlFile, 0775, true);
+            umask($old);
+            if (!$result) {
+                set_user_message("Sorry, impossible to create directory '".plainstring_to_htmlprotected($baseDirDownSqlFile)."'. Please check directory permissions.");
+                //header('Location: .?name='.urlencode($name).'&selfedit='.urlencode($selfedit).($patchInstanceName ? '&patchInstanceName='.$patchInstanceName : ''));
+
+                return;
+            }
+        }
+
+        if (!is_writable($baseDirDownSqlFile)) {
+            set_user_message("Sorry, directory '".plainstring_to_htmlprotected($baseDirDownSqlFile)."' is not writable. Please check directory permissions.");
+            //header('Location: .?name='.urlencode($name).'&selfedit='.urlencode($selfedit).($patchInstanceName ? '&patchInstanceName='.$patchInstanceName : ''));
+
+            return;
+        }
+
+        file_put_contents($rootPath.$upSqlFileName, $upSql);
+        // Chmod may fail if the file does not belong to the Apache user.
+        @chmod($rootPath.$upSqlFileName, 0664);
+
+        file_put_contents($rootPath.$downSqlFileName, $downSql);
+        // Chmod may fail if the file does not belong to the Apache user.
+        @chmod($rootPath.$downSqlFileName, 0664);
     }
 }
--- src/views/cmsGenerator.php
+++ src/views/cmsGenerator.php
@@ -1,3 +1,21 @@
-<?php
+<?php /* @var $this CmsGeneratorController */ ?>
+<h1>Generate a new CMS component</h1>
 
-echo '<p>Hello world !</p>';
+<p>By clicking the link below, you will automatically generate a database patch containing a table creation request.</p>
+
+<form action="componentGenerate" method="post" class="form-horizontal">
+    <input type="hidden" id="name" name="name" value="<?php echo plainstring_to_htmlprotected($this->instanceName) ?>" />
+    <input type="hidden" id="selfedit" name="selfedit" value="<?php echo plainstring_to_htmlprotected($this->selfedit) ?>"
+    <div class="control-group">
+        <label class="control-label">Component name :</label>
+        <div class="controls">
+            <input type="text" name="componentName">
+            <span class="help-block">The name for the CMS component. This name will be the name of the table generated in the database patch.</span>
+        </div>
+    </div>
+    <div class="control-group">
+        <div class="controls">
+            <button name="action" value="componentGenerate" type="submit" class="btn btn-danger">Generate patch</button>
+        </div>
+    </div>
+</form>
