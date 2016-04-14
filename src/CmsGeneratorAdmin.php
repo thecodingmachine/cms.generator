cdfd6f20375bf6331602e89fe0cdf6c791774637
jbc <jb.charron@thecodingmachine.com>
2016-04-14 11:10:57 +0200

    WIP

--- src/CmsGeneratorAdmin.php
+++ src/CmsGeneratorAdmin.php
@@ -5,7 +5,7 @@ use Mouf\MoufUtils;
 
 MoufUtils::registerMainMenu('cmsMainMenu', 'CMS', null, 'mainMenu', 70);
 MoufUtils::registerMenuItem('cmsSubMenu', 'Generator', null, 'cmsMainMenu', 80);
-MoufUtils::registerMenuItem('cmsGeneratorSubSubMenu', 'Generate new CMS component', 'generateCmsComponent/', 'cmsSubMenu', 10);
+MoufUtils::registerMenuItem('cmsGeneratorSubSubMenu', 'Generate new CMS component', 'cmsadmin/', 'cmsSubMenu', 10);
 
 // Controller declaration
 $moufManager = MoufManager::getMoufManager();
